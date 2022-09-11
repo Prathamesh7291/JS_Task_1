3943aae4462eb30d5620edb11a987b9f5efe3335
Prathamesh <prathameshkalkute@gmail.com>
2022-09-11 05:34:56 +0530

    Comments added for explaination.

--- jssrc.js
+++ jssrc.js
@@ -1,9 +1,19 @@
 const bgcol = () => {
     const random_col = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + random_col;
-    hex_cd.innerHTML = "#" + random_col;
+    document.body.style.backgroundColor = "#" + random_col; //Used to change the background color into random color generated.
+    hex_cd.innerHTML = "#" + random_col; // Used to display the hexcode.
   }
   
-  btn.addEventListener("click", bgcol);
-  bgcol();
+  btn.addEventListener("click", bgcol); // on clicking it will execute the bgcol function.
 
+  bgcol(); //Call
+
+/* 
+Function Explaination :
+1. Math.random returns a floating point random number upto the max value - #FFFFFF i.e 16777215.
+
+2. Math.floor is used to convert this to an integer.
+
+3. Now using toString(16) we convert the integer to HEX again.
+
+*/
